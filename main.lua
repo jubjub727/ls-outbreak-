4aa783fbb3ad29580d43bdfbfa1b5ff919caf279
jubjub <matthewbrockgosnell@gmail.com>
2017-06-19 06:11:05 +1200

    Initial Files

---
+++ main.lua
@@ -0,0 +1,78 @@
+--[[
+
+DayZ style gamemode by jubjub and vaffanculo!
+Steam - http://steamcommunity.com/id/jubjub727/
+
+Feel free to add me for support but I'll block you if you're annoying.
+
+]]--
+
+
+-- Config
+
+local WeaponTypes = {"weapon_bat", "weapon_pistol50", "weapon_combatpdw", "weapon_microsmg", "weapon_assaultshotgun", "weapon_carbinerifle", "weapon_assaultrifle", "weapon_flare", "weapon_sniperrifle"}
+
+-- End Config
+
+-- Advanced
+
+-- End Advanced
+
+
+-- Actual Code
+
+AddClientScript("playerMain.lua")
+
+local sha2 = dofile("sha2.lua")
+
+local conn
+
+local Players = {}
+
+Player:On("connect", function(ply) -- When a player connects add them to our table
+    local username, password
+    -- Get User Input Here
+    if (Login(ply, username, password)) then
+        ply:setPosition(0,0,0)
+    else
+        ply:kick()
+    end
+end )
+
+Player:On("disconnect", function(ply) -- When a player disconnects go through our table and find them and remove them from our table
+    for k,v in pairs(Players) do
+        if ply:getID() == v[2]:getID() then
+            table.remove(Players, k)
+        end
+    end
+end )
+
+local function loadMySQL()
+    print("Initialising MySQL")
+    conn = MySQL:Connect("27.100.36.9", "outbreak", "orangetest", "fucknativeui")
+
+    -- Initial Setup if no tables exist
+    conn.query("CREATE TABLE IF NOT EXISTS users( uid int NOT NULL, username varchar(256) NOT NULL, password varchar(64));")
+    print("Done Initialising MySQL")
+end
+
+local function Login(ply, username, password)
+    local passHash = sha2.hash256(password)
+
+    local result = conn.query("SELECT uid, username, password FROM users WHERE username = '%s' AND password = '%s';", username, passHash)
+    if #result < 1 then
+        return 0
+    else
+        table.insert(Players, {result.uid, ply})
+        return 1
+    end
+end
+
+local function Setup() -- Called before anything for things that need to be setPosition
+    
+    loadMySQL() -- Initialise any MySQL related features
+
+
+end
+
+Setup()
